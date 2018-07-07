90ea535728a7d3c18b6f1283e3ee6601e3112049
Kazuki74 <allykakki@me.com>
2018-07-07 16:29:24 +0900

    implemented status-toggle functionality

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,4 @@
 class Blog < ApplicationRecord
 	enum status: { draft: 0 , published: 1 }
+	default_scope { order(id: :desc) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
-  resources :blogs
+  resources :blogs do
+  	member do
+  		get :toggle_status
+  	end
+  end
   resources :portfolios
 end
