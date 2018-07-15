69f9baee02a08cfb3953c76ac3bc28b0f5775f69
Kazuki74 <allykakki@me.com>
2018-07-15 14:12:56 +0900

    Inplemented concern helper

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,9 @@
 class Skill < ApplicationRecord
-	validates_presence_of :title, :percent_utilize
+	include Placeholder
+	validates_presence_of :title, :percent_utilized
+	after_initialize :set_default
+
+	def set_default
+		self.badge ||= Placeholder.image_generator(height: "250", width: "250")
+	end
 end
